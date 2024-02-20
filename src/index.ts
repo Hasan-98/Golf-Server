fb7419c2a46cbbbb2c2a0b2aac85a4904fb85038
hasanmahmood <hassan.mahmood@emumba.com>
2024-02-20 20:47:27 +0500

    cors added

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { app } from './app';
-const socketio = require('socket.io');
+import { app } from "./app";
+const socketio = require("socket.io");
 const port = process.env.PORT || 5000;
-app.set('port', port);
-
-
+app.set("port", port);
 const test = app.listen(port, () => {
   console.log(`Listening: http://localhost:${port}`);
 });
-const io = socketio(test);
-io.on('connection', (socket: any) => {
-  console.log('New client connected');
+const corsOptions = {
+  origin: `http://localhost:${port}`,
+  methods: ["GET", "POST"],
+  allowedHeaders: ["my-custom-header"],
+  credentials: true,
+};
+const io = socketio(test, {
+  cors: corsOptions,
 });
-
-export { io }
+io.on("connection", (socket: any) => {
+  console.log("New client connected" + socket);
+});
+export { io };
