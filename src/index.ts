da59ee72cac3c2b64d1162f220886114adaa646c
hasanmahmood <hassan.mahmood@emumba.com>
2024-02-26 23:13:50 +0500

    cors added

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const test = app.listen(port, () => {
   console.log(`Listening: http://localhost:${port}`);
 });
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "http://golf-encounters.com"],
   methods: ["GET", "POST"],
   allowedHeaders: ["my-custom-header"],
   credentials: true,
