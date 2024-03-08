0b57a69b0b27ac7f7a69d5f44d0484e5bad231cf
hasanmahmood <hassan.mahmood@emumba.com>
2024-03-08 20:21:22 +0500

    post update fixed

--- src/api/postController.ts
+++ src/api/postController.ts
@@ -135,16 +135,11 @@ export const getPostById: RequestHandler = async (req, res, next) => {
 export const updatePost: RequestHandler = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const { category, tags, text } = req.body;
     const post = await models.Post.findOne({ where: { id } });
     if (!post) {
       return res.status(404).json({ error: "Post not found" });
     }
-    const updatedPost = await post.update({
-      category,
-      tags,
-      text,
-    });
+    const updatedPost = await post.update(req.body);
     res.status(200).json({
       message: "Post updated successfully",
       updatedPost,
