c9f4eb9855d53be5c5387b1ad8e30392aafce2aa
hasanmahmood <hassan.mahmood@emumba.com>
2024-04-26 00:27:43 +0500

    updated

--- src/api/appointmentController.ts
+++ src/api/appointmentController.ts
@@ -144,7 +144,7 @@ export const acceptAppointment: RequestHandler = async (
               endTime,
               isBooked: true,
               bookedBy: studentId,
-              status: "BOOKED",
+              status: "PENDING",
             },
           }
         );
@@ -398,7 +398,7 @@ export const updateAppointmentStatus: RequestHandler = async (
               endTime,
               isBooked: true,
               bookedBy: userId,
-              status: "BOOKED",
+              status: "PENDING",
             },
           }
         );
