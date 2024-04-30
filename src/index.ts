6ddb35353e86fe60174050db8863f7eee7b86d98
Hasan-98 <hasanmehmood458@gmail.com>
2024-04-30 23:07:53 +0500

    Certificate added

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,37 @@
 import { app } from "./app";
+import https from 'https';
+import fs from 'fs';
 const socketio = require("socket.io");
+
 const port = process.env.PORT || 5000;
 app.set("port", port);
-const test = app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
+
+// Read the SSL certificate and private key from files
+const privateKey = fs.readFileSync('./certificate/key.pem', 'utf8');
+const certificate = fs.readFileSync('./certificate/cert.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+
+// Create an HTTPS server
+const server = https.createServer(credentials, app);
+
+server.listen(port, () => {
+  console.log(`Listening: https://localhost:${port}`);
 });
+
 const corsOptions = {
   origin: ["http://localhost:3000", "https://golf-encounters.com"],
   methods: ["GET", "POST"],
   allowedHeaders: ["my-custom-header"],
   credentials: true,
 };
-const io = socketio(test, {
+
+const io = socketio(server, {
   cors: corsOptions,
 });
+
 io.on("connection", (socket: any) => {
   console.log("New client connected" + socket);
 });
+
 export { io };
