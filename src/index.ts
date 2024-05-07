e46a0e7c9f5c52d449284b4bccc1ba4c5841fa37
Hasan-98 <hasanmehmood458@gmail.com>
2024-05-07 09:43:59 +0500

    conflicts removed

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { app } from "./app";
 const socketio = require("socket.io");
-<<<<<<< HEAD
 const port = process.env.PORT || 5000;
 app.set("port", port);
 const test = app.listen(port, () => {
   console.log(`Listening: http://localhost:${port}`);
-=======
-const port = process.env.PORT || 5002;
-app.set("port", port);
-const test = app.listen(port, () => {
-  console.log(`Listening: http://admin.golf-encounters.com/:${port}`);
->>>>>>> parent of e89cabf (chnges)
 });
 const corsOptions = {
   origin: ["http://localhost:3000", "https://golf-encounters.com"],
