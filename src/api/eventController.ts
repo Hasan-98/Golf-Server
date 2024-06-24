2a11348761dbe645f8d89eeeec5ea9c847cdec1a
hasanemumba <hassan.mahmood@emumba.com>
2024-06-24 19:18:06 +0500

    NOtfication updated

--- src/api/eventController.ts
+++ src/api/eventController.ts
@@ -615,6 +615,7 @@ export const updateNotificationResponse: RequestHandler = async (req, res) => {
       { isRead: true, message},
       { where: { id: notificationId} }
     );
+    return res.status(200).json({ message: "Request processed successfully" });
   } catch (error) {
     console.error("Error:", error);
     return res.status(500).json({ error: "Cannot approve join request at the moment" });
